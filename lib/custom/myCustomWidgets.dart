beaee6b1f7a3d5316c959ef0a84ebef213a143b6
MezzLasha <mezzlasha@gmail.com>
2022-12-10 12:21:04 +0400

    remove scaling button

--- lib/custom/myCustomWidgets.dart
+++ lib/custom/myCustomWidgets.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_custom_tabs/flutter_custom_tabs.dart';
 
 // import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
-import 'package:scale_button/scale_button.dart' as scl;
 import 'package:url_launcher/url_launcher.dart' as url_launcher;
 
 void showSnackBar(BuildContext context, String message) {
@@ -12,63 +11,6 @@ void showSnackBar(BuildContext context, String message) {
     behavior: SnackBarBehavior.fixed,
   );
   ScaffoldMessenger.of(context).showSnackBar(snackbar);
-}
-
-class CircularButton extends StatelessWidget {
-  final Widget icon;
-  final void Function() onPressed;
-  final double? radius;
-  final Color? backgroundColor;
-
-  const CircularButton(
-      {Key? key,
-      required this.icon,
-      required this.onPressed,
-      this.radius,
-      this.backgroundColor})
-      : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return scl.ScaleButton(
-      bound: 0.07,
-      duration: const Duration(milliseconds: 150),
-      child: CircleAvatar(
-        radius: radius ?? 30,
-        backgroundColor: backgroundColor ?? const Color.fromARGB(12, 0, 0, 0),
-        child: SizedBox(
-          width: radius != null ? radius! * 2 : 60,
-          height: radius != null ? radius! * 2 : 60,
-          child: IconButton(
-            color: Theme.of(context).primaryColor,
-            icon: icon,
-            onPressed: onPressed,
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class ScalingButton extends StatelessWidget {
-  final Widget child;
-  final Function? onPressed;
-
-  const ScalingButton({super.key, required this.child, this.onPressed});
-
-  @override
-  Widget build(BuildContext context) {
-    return scl.ScaleButton(
-      bound: 0.07,
-      onTap: () {
-        if (onPressed != null) {
-          onPressed!();
-        }
-      },
-      duration: const Duration(milliseconds: 150),
-      child: child,
-    );
-  }
 }
 
 class DragHandlePill extends StatelessWidget {
