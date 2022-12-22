b317303ab77787a64a650aca948f8b077b3c6f7b
MezzLasha <mezzlasha@gmail.com>
2022-12-22 23:12:19 +0400

    settings screen reorganize, add bug report and ask for feature field

--- lib/presentation/settings_screen.dart
+++ lib/presentation/settings_screen.dart
@@ -1,9 +1,6 @@
-import 'package:dynamic_color/dynamic_color.dart';
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:launch_review/launch_review.dart';
 import 'package:vacancy_scraper/custom/myCustomWidgets.dart';
-import 'package:vacancy_scraper/main.dart';
 
 class SettingsScreen extends StatefulWidget {
   const SettingsScreen({super.key});
@@ -38,12 +35,6 @@ class _SettingsScreenState extends State<SettingsScreen> {
             },
             child: ListTile(
               title: const Text('ენა'),
-              leading: Icon(
-                Icons.translate,
-                color: const CustomColors(danger: Colors.lightBlueAccent)
-                    .harmonized(Theme.of(context).colorScheme)
-                    .danger,
-              ),
               trailing: Row(
                 mainAxisSize: MainAxisSize.min,
                 children: [
@@ -69,12 +60,15 @@ class _SettingsScreenState extends State<SettingsScreen> {
           ),
           ListTile(
             title: const Text('Play Store-ზე შეფასება'),
-            leading: Icon(Icons.rate_review_outlined,
-                color: const CustomColors(danger: Colors.greenAccent)
-                    .harmonized(Theme.of(context).colorScheme)
-                    .danger),
             onTap: () => LaunchReview.launch(),
           ),
+          ListTile(
+              title: const Text('ფუნქციის მოთხოვნა'),
+              onTap: () =>
+                  sendEmail('ფუნქციის მოთხოვნა', 'mezzlasha@gmail.com', '')),
+          ListTile(
+              title: const Text('Bug report'),
+              onTap: () => sendEmail('Bug report', 'mezzlasha@gmail.com', '')),
         ]))
       ],
     ));
